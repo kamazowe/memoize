f30a677accc88d8b0a2a2d2674ba80b49567e3bb
kgebala <kamil.gebala@valueadd.pl>
2022-06-29 17:54:18 +0200

    changes 3

--- apps/memo/src/app/nx-welcome.component.ts
+++ apps/memo/src/app/nx-welcome.component.ts
@@ -433,7 +433,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome memo 👋
+            Welcome memo 2 👋
           </h1>
         </div>
 
